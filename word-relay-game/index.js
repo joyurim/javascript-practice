83ae1682db9f37c815dd7ed9f53c083fde9af922
조유림 <joyurim1989@10x10.co.kr>
2023-08-01 17:17:41 +0900

    레이어 구조 변경

--- word-relay-game/index.js
+++ word-relay-game/index.js
@@ -1,38 +1,46 @@
 const number = parseInt(prompt('몇 명이 참가하나요?'));
-const $button = document.querySelector('button');
-const $input = document.querySelector('input');
-const $word = document.querySelector('#word');
-const $order = document.querySelector('#order');
 
-let word; // 제시어
-let newWord; // 새로 입력한 단어
+if(number) {
+    console.log(number, '111')
+    const $button = document.querySelector('button');
+    const $input = document.querySelector('input');
+    const $word = document.querySelector('#word');
+    const $order = document.querySelector('#order');
 
-const onClickButton = () => {
-    if(!word || word[word.length - 1] === newWord[0]) {
-        wordInputFunction();
-    } else {
-        alert('게임종료!\n올바르지 않은 단어입니다.');
+    let word; // 제시어
+    let newWord; // 새로 입력한 단어
+
+    const onClickButton = () => {
+        if(newWord.length === 3 && (!word || word[word.length - 1] === newWord[0])) {
+            wordInputFunction();
+        } else {
+            alert('게임종료!\n올바르지 않은 단어입니다.');
+            $input.value = '';
+            $word.textContent = '';
+            $input.focus();
+            $order.textContent = 1;
+        }
+    }
+    const wordInputFunction = () => {
+        word = newWord; // 입력한 단어가 제시어가 됨
+        $word.textContent = word;
         $input.value = '';
-        $word.textContent = '';
+
+        const order = parseInt($order.textContent); // 현제 순서
+        if(order + 1 > number) {
+            $order.textContent = 1;
+        } else {
+            $order.textContent = order + 1;
+        }
+
+        $input.value = '';
         $input.focus();
-        $order.textContent = 1;
     }
-}
-const wordInputFunction = () => {
-    word = newWord;
-    $word.textContent = word;
-    $input.value = '';
-    const order = parseInt($order.textContent);
-    if(order + 1 > number) {
-        $order.textContent = 1;
-    } else {
-        $order.textContent = order + 1;
+    const onInput = (event) => {
+        newWord = event.target.value;
     }
-    $input.value = '';
-    $input.focus();
+    $button.addEventListener('click', onClickButton);
+    $input.addEventListener('input', onInput)
+} else {
+    console.log(number, '222')
 }
-const onInput = (event) => {
-    newWord = event.target.value;
-}
-$button.addEventListener('click', onClickButton);
-$input.addEventListener('input', onInput)
